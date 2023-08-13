b1f861190bf81f9c067554a00dd2baf7a312dcb1
Londonteam <142154425+Londonteam@users.noreply.github.com>
2023-08-13 19:06:59 +0100

    Add files via upload

--- POC_ZOBIATICS.py
+++ POC_ZOBIATICS.py
@@ -8,17 +8,36 @@ Created on Sun Aug 13 07:42:45 2023
 import pandas as pd
 
 file_path = 'C:/Users/personal/Desktop/Personal/customers.csv'  
-df = pd.read_csv(file_path)
+customers_data = pd.read_csv(file_path)
 
 file_path1 = 'C:/Users/personal/Desktop/Personal/products.csv'  
-df1 = pd.read_csv(file_path1)
+products_data = pd.read_csv(file_path1)
 
 file_path2 = 'C:/Users/personal/Desktop/Personal/purchases.csv'  
-df2 = pd.read_csv(file_path2)
+purchases_data = pd.read_csv(file_path2)
 
 # Display the first few rows of the DataFrame
-print(df.head())
+print(customers_data.head())
 
-print(df1.head())
+print(products_data.head())
 
-print(df2.head())
+print(purchases_data.head())
+
+# Display basic statistics about numerical columns using describe()
+customers_data.describe()
+
+products_data.describe()
+
+purchases_data.describe()
+
+# Display summary information using the .info() method
+customers_data.info() 
+
+products_data.info()
+
+purchases_data.info()
+
+
+
+
+
